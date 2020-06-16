654d157d077b4ab2e66b0b5e0ec82690bbc258dc
Markus Maurer <design@markusmaurer.at>
2020-06-16 08:10:05 +0200

    Initial Commit

---
+++ CovidApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CovidApp
+//
+//  Created by ic iMac on 16.06.20 : 25.
+//  Copyright © 2020 interactive curiosity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
